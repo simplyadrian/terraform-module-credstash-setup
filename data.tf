8045a1625d420f27881c155feb1a8073eed3d856
Adrian Herrera <simplyadrian@gmail.com>
2018-11-06 14:39:00 -0600

    first commit

---
+++ data.tf
@@ -0,0 +1,51 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+data "aws_partition" "current" {}
+
+data "template_file" "credstash-get-cmd" {
+  template = "env credstash -r ${data.aws_region.current.name} -t ${var.db_table_name} get"
+}
+
+data "template_file" "credstash-put-cmd" {
+  template = "env credstash -r ${data.aws_region.current.name} -t ${var.db_table_name} put -k ${aws_kms_alias.credstash-key.name}"
+}
+
+data "template_file" "credstash-install-snippet" {
+  template = <<END_TEMPLATE
+{ apt-get update;
+  apt-get install -y build-essential libssl-dev libffi-dev python-dev python-pip;
+  pip install --upgrade pip==9.0.3;
+  pip install credstash; }
+END_TEMPLATE
+}
+
+# Writer Policy
+data "aws_iam_policy_document" "writer-policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["dynamodb:PutItem"]
+
+    resources = [
+      "arn:${data.aws_partition.current.partition}:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/${var.db_table_name}",
+    ]
+  }
+}
+
+# Reader Policy
+data "aws_iam_policy_document" "reader-policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:Query",
+      "dynamodb:Scan",
+    ]
+
+    resources = [
+      "arn:${data.aws_partition.current.partition}:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/${var.db_table_name}",
+    ]
+  }
+}
---
+++ main.tf
@@ -0,0 +1,64 @@
+/**
+ * ## Credstash Setup
+ *
+ * Setup KMS Master Key and a DynamoDB Table for use with Credstash.
+ *
+ * By default, this module doesn't need any variables to be set manually, but can
+ * be overridden if necessary. By doing so it is possible to create either key or
+ * database table or both, as well as other customizations.
+ *
+ * **Resources created here cannot be deleted using terraform and have to be deleted
+ * manually. This behavior is to prevent possibility of sensitive data loss.**
+ *
+ */
+
+resource "aws_kms_key" "credstash-key" {
+  count               = "${var.create_kms_key ? 1 : 0}"
+  description         = "Master key used by credstash"
+  enable_key_rotation = "${var.enable_key_rotation}"
+
+  tags = {
+    Name = "${var.kms_key_name}"
+  }
+}
+
+resource "aws_kms_alias" "credstash-key" {
+  count         = "${var.create_kms_key ? 1 : 0}"
+  name          = "alias/${var.kms_key_name}"
+  target_key_id = "${aws_kms_key.credstash-key.key_id}"
+}
+
+resource "aws_dynamodb_table" "credstash-db" {
+  count          = "${var.create_db_table ? 1 : 0}"
+  name           = "${var.db_table_name}"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "name"
+  range_key      = "version"
+
+  attribute {
+    name = "name"
+    type = "S"
+  }
+
+  attribute {
+    name = "version"
+    type = "S"
+  }
+}
+
+## Writer Policy
+
+resource "aws_iam_policy" "writer-policy" {
+  count  = "${var.create_writer_policy ? 1 : 0}"
+  name   = "${var.db_table_name}-writer"
+  policy = "${data.aws_iam_policy_document.writer-policy.json}"
+}
+
+## Reader Policy
+
+resource "aws_iam_policy" "reader-policy" {
+  count  = "${var.create_reader_policy ? 1 : 0}"
+  name   = "${var.db_table_name}-reader"
+  policy = "${data.aws_iam_policy_document.reader-policy.json}"
+}
